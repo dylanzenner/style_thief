372d773ca3e40a209eeabfc13cb0631581336003
Dylan Zenner <dzenner@1board.io>
2022-08-07 18:14:27 -0500

    update files

--- backend/app/api.py
+++ backend/app/api.py
@@ -6,7 +6,19 @@ from typing import List
 from starlette.responses import StreamingResponse
 import cv2
 import numpy as np
-from style import resize_images
+from style import resize_images, VGG19
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from PIL import Image
+
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+import copy
 
 
 
@@ -27,14 +39,63 @@ app.add_middleware(
 
 @app.post("/upload")
 async def receive_file(base_image: bytes = File(), style_image: bytes = File()):
-    image1 = Image.open(io.BytesIO(base_image))
-    image2 = Image.open(io.BytesIO(style_image))
+    # load the images
+    print(base_image)
+    base_img = Image.open(io.BytesIO(base_image))
+    style_img = Image.open(io.BytesIO(style_image))
 
-    image1_resized, image2_resized = resize_images(image1, image2)
-    print(image1_resized, image2_resized)
+    # resize the images to be 128
+    base_img_resized, style_img_resized = resize_images(base_img, style_img)
+    
+    # generated a noise image
+    noise_img = torch.randn(base_img_resized.data.shape, device='cpu', requires_grad=True)
+    
+    # instantiate the model
+    model = VGG19().to('cpu').eval()
+
+
+    total_steps = 6000
+    learning_rate = 0.001
+    alpha = 1
+    beta = 0.01
+    optimizer = optim.Adam([noise_img], lr = learning_rate)
+
+    for step in range(total_steps):
+        noise_features = model(noise_img)
+        base_features = model(base_img_resized)
+        style_features = model(style_img_resized)
+
+        style_loss = 0
+        base_loss = 0
+        print('Starting: {}'.format(step))
+        for noise_feature, base_feature, style_feature in zip(noise_features, base_features, style_features):
+            _, c, h, w = noise_feature.shape
+            
+            base_loss += torch.mean((noise_feature - base_feature) ** 2)
+
+            G = noise_feature.view(c, h * w).mm(noise_feature.view(c, h * w).t())
+            A = style_feature.view(c, h * w).mm(style_feature.view(c, h * w).t())
+
+            style_loss += torch.mean((G - A) ** 2)
+
+        total_loss = alpha * base_loss + beta * style_loss
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
+        if step % 200 == 0:
+            stylized_img = torch.squeeze(noise_img)
+            print(total_loss)
+            print(stylized_img.shape)
+            print('YESSS')
+
+    final = transforms.ToPILImage()(stylized_img) # make sure tensor is on cpu
+    final.show()
+
+
     bytes_image = io.BytesIO()
-    image1.save(bytes_image, "JPEG")
+    final.save(bytes_image, 'JPEG')
     bytes_image.seek(0)
+
+
     return StreamingResponse(content=bytes_image, media_type="image/jpeg")
 
-    # return StreamingResponse(io.BytesIO(image1.tobytes()), media_type="image/jpeg")
--- backend/app/style.py
+++ backend/app/style.py
@@ -19,24 +19,41 @@ def resize_images(img_1, img_2, image_size = 128):
     device = torch.device('cpu')
 
     loader = transforms.Compose([
-        transforms.Resize(image_size), 
-        transforms.ToTensor()
+        transforms.Resize((image_size, image_size)), 
+        transforms.ToTensor(),
+        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
     
     resized_img_1, resized_img_2 = loader(img_1).unsqueeze(0), loader(img_2).unsqueeze(0)
-    return (resized_img_1.to(device, torch.float), resized_img_2.to(device, torch.float))
+    return (resized_img_1.to(device), resized_img_2.to(device))
 
-def content_loss():
-    pass
 
-def style_loss():
-    pass
+class VGG19(nn.Module):
+    def __init__(self):
+        super(VGG19, self).__init__()
+        self.chosen_features = ["0", "5", "10", "19", "28"]
+        self.model = models.vgg19(pretrained=True).features[:29]
 
-def gram_matrix():
-    pass
 
-def style_transfer():
-    pass
+    def forward(self, x):
+
+        features = []
+
+        # Go through each layer in model, if the layer is in the chosen_features,
+        # store it in features. At the end we'll just return all the activations
+        # for the specific layers we have in chosen_features
+        for layer_num, layer in enumerate(self.model):
+            x = layer(x)
+
+            if str(layer_num) in self.chosen_features:
+                features.append(x)
+
+        return features
+
+
+
+
+    
 
 
     
