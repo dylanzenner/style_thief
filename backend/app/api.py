0620b41afe369bb9adcc9e3dc7de762a4a928f78
Dylan Zenner <dzenner@1board.io>
2022-08-13 11:15:51 -0500

    update project

--- backend/app/api.py
+++ backend/app/api.py
@@ -6,7 +6,7 @@ from typing import List
 from starlette.responses import StreamingResponse
 import cv2
 import numpy as np
-from style import resize_images, VGG19
+from style import VGG19, content_loss
 
 import torch
 import torch.nn as nn
@@ -40,62 +40,82 @@ app.add_middleware(
 @app.post("/upload")
 async def receive_file(base_image: bytes = File(), style_image: bytes = File()):
     # load the images
-    print(base_image)
     base_img = Image.open(io.BytesIO(base_image))
     style_img = Image.open(io.BytesIO(style_image))
 
-    # resize the images to be 128
-    base_img_resized, style_img_resized = resize_images(base_img, style_img)
+    # resize the images to 224 x 224
+    resize_image = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    base_img_resized = resize_image(base_img).unsqueeze(0).to('cpu')
+    style_img_resized = resize_image(style_img).unsqueeze(0).to('cpu')
     
     # generated a noise image
     noise_img = torch.randn(base_img_resized.data.shape, device='cpu', requires_grad=True)
     
     # instantiate the model
-    model = VGG19().to('cpu').eval()
+    model = VGG19()
+
+    print(content_loss(model, base_img_resized ,content_layer=0))
 
 
-    total_steps = 6000
-    learning_rate = 0.001
-    alpha = 1
-    beta = 0.01
-    optimizer = optim.Adam([noise_img], lr = learning_rate)
 
-    for step in range(total_steps):
-        noise_features = model(noise_img)
-        base_features = model(base_img_resized)
-        style_features = model(style_img_resized)
 
-        style_loss = 0
-        base_loss = 0
-        print('Starting: {}'.format(step))
-        for noise_feature, base_feature, style_feature in zip(noise_features, base_features, style_features):
-            _, c, h, w = noise_feature.shape
+
+    # total_steps = 100
+    # learning_rate = 0.001
+    # alpha = 1
+    # beta = 0.01
+    # optimizer = optim.Adam([noise_img], lr = learning_rate)
+
+    # for step in range(total_steps):
+    #     noise_features = model(noise_img)
+    #     base_features = model(base_img_resized)
+    #     style_features = model(style_img_resized)
+
+    #     style_loss = 0
+    #     base_loss = 0
+    #     print('Starting: {}'.format(step))
+    #     for noise_feature, base_feature, style_feature in zip(noise_features, base_features, style_features):
+    #         _, c, h, w = noise_feature.shape
             
-            base_loss += torch.mean((noise_feature - base_feature) ** 2)
+    #         base_loss += torch.mean((noise_feature - base_feature) ** 2)
 
-            G = noise_feature.view(c, h * w).mm(noise_feature.view(c, h * w).t())
-            A = style_feature.view(c, h * w).mm(style_feature.view(c, h * w).t())
+    #         G = noise_feature.view(c, h * w).mm(noise_feature.view(c, h * w).t())
+    #         A = style_feature.view(c, h * w).mm(style_feature.view(c, h * w).t())
 
-            style_loss += torch.mean((G - A) ** 2)
+        #     style_loss += torch.mean((G - A) ** 2)
 
-        total_loss = alpha * base_loss + beta * style_loss
-        optimizer.zero_grad()
-        total_loss.backward()
-        optimizer.step()
-        if step % 200 == 0:
-            stylized_img = torch.squeeze(noise_img)
-            print(total_loss)
-            print(stylized_img.shape)
-            print('YESSS')
-
-    final = transforms.ToPILImage()(stylized_img) # make sure tensor is on cpu
-    final.show()
+        # total_loss = alpha * base_loss + beta * style_loss
+        # optimizer.zero_grad()
+        # total_loss.backward()
+        # optimizer.step()
+        # if step % 20 == 0:
+        #     denorm = transforms.Normalize((-2.12, -2.04, -1.80), (4.37, 4.46, 4.44))
+        #     stylized_img = noise_img.clone().squeeze()
+        #     stylized_img = denorm(stylized_img).clamp_(0, 1)
+        #     final = transforms.ToPILImage()(stylized_img) # make sure tensor is on cpu
+        #     final.show()
 
 
-    bytes_image = io.BytesIO()
-    final.save(bytes_image, 'JPEG')
-    bytes_image.seek(0)
+        #     bytes_image = io.BytesIO()
+        #     final.save(bytes_image, 'JPEG')
+        #     bytes_image.seek(0)
+        #     print(total_loss)
+        #     print(stylized_img.shape)
+        #     print('YESSS')
+
+    # final = transforms.ToPILImage()(stylized_img) # make sure tensor is on cpu
+    # final.show()
 
 
-    return StreamingResponse(content=bytes_image, media_type="image/jpeg")
+    # bytes_image = io.BytesIO()
+    # final.save(bytes_image, 'JPEG')
+    # bytes_image.seek(0)
+
+
+    # return StreamingResponse(content=bytes_image, media_type="image/jpeg")
 
--- backend/app/style.py
+++ backend/app/style.py
@@ -4,50 +4,84 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
 from PIL import Image
-
 import torchvision.transforms as transforms
 import torchvision.models as models
 
-import copy
 
 
-
-def resize_images(img_1, img_2, image_size = 128):
-
-    device = torch.device('cpu')
+def resize_image(img, image_size = 224):
 
     loader = transforms.Compose([
         transforms.Resize((image_size, image_size)), 
         transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
     
-    resized_img_1, resized_img_2 = loader(img_1).unsqueeze(0), loader(img_2).unsqueeze(0)
-    return (resized_img_1.to(device), resized_img_2.to(device))
+    resized_img = loader(img).unsqueeze(0).to('cpu')
+    return resized_img
+
+def content_loss(model, content_img, content_layer=4, epochs=1000, lr=1e-1):
+    content_features = model(content_img)
+    img = 1e-1*torch.randn_like(content_img)
+    img.requires_grad_(True)
+
+    opt = torch.optim.Adam([img], lr = lr)
+
+    for epoch in range(epochs):
+        print('epoch: {}'.format(epoch))
+        opt.zero_grad()
+        features = model(img)
+        content = features[content_layer]
+        content_target = content_features[content_layer].detatch()
+        loss = ((content - content_target) ** 2).sum()
+        loss.backward()
+        opt.step()
+    return img
+
+
+
+
+
+
+
+
+
+
+
+
+def training_loop(model, base_img, base_img_layer=4, epochs=1000, lr=1e-1):
+
+    base_features = model(base_img)
+    noise_img = torch.randn(base_img.data.shape, device='cpu', requires_grad=True)
+    optimizer = optim.Adam([noise_img], lr = lr)
+
+    for epoch in range(epochs):
+        optimizer.zero_grad()
+        features = model(noise_img)
+        content = features[base_img_layer]
+        content_target = base_features[base_img_layer].detatch()
+        loss = ((content - content_target) ** 2).sum()
+        loss.backward()
+        optimizer.step()
+    return resize_images(noise_img)
+
 
 
 class VGG19(nn.Module):
     def __init__(self):
         super(VGG19, self).__init__()
-        self.chosen_features = ["0", "5", "10", "19", "28"]
-        self.model = models.vgg19(pretrained=True).features[:29]
+        self.model = models.vgg19(pretrained=True).eval().to('cpu')
 
 
     def forward(self, x):
 
         features = []
 
-        # Go through each layer in model, if the layer is in the chosen_features,
-        # store it in features. At the end we'll just return all the activations
-        # for the specific layers we have in chosen_features
-        for layer_num, layer in enumerate(self.model):
+        for layer in self.model.features:
             x = layer(x)
-
-            if str(layer_num) in self.chosen_features:
+            if type(x) == nn.Conv2d:
                 features.append(x)
-
         return features
 
 
