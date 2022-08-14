4bf1ce831834c14ed9fc95c878a4f703d422af27
Dylan Zenner <dzenner@1board.io>
2022-08-14 18:58:05 -0500

    update project

--- backend/app/api.py
+++ backend/app/api.py
@@ -4,22 +4,29 @@ from PIL import Image
 import io
 from typing import List
 from starlette.responses import StreamingResponse
-import cv2
 import numpy as np
-from style import VGG19, content_loss
+import time
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from PIL import Image
-
 import torchvision.transforms as transforms
 import torchvision.models as models
 
-import copy
-
+class VGG19(nn.Module):
+    def __init__(self):
+        super(VGG19, self).__init__()
+        self.model = models.vgg19(pretrained=True)
+    
+    def forward(self, img):
+        features = []
+        for layer in self.model.features:
+            img = layer(img)
+            if type(layer) == nn.Conv2d:
+                features.append(img)
+        return features
 
 
 app = FastAPI()
@@ -39,83 +46,68 @@ app.add_middleware(
 
 @app.post("/upload")
 async def receive_file(base_image: bytes = File(), style_image: bytes = File()):
+    start_time = time.time()
     # load the images
     base_img = Image.open(io.BytesIO(base_image))
     style_img = Image.open(io.BytesIO(style_image))
 
-    # resize the images to 224 x 224
+    # resize the images to 224 x 224 and normalize
     resize_image = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
+    # unsqueeze the images to add another dimension for batch size and use the CPU
     base_img_resized = resize_image(base_img).unsqueeze(0).to('cpu')
     style_img_resized = resize_image(style_img).unsqueeze(0).to('cpu')
-    
-    # generated a noise image
-    noise_img = torch.randn(base_img_resized.data.shape, device='cpu', requires_grad=True)
-    
-    # instantiate the model
-    model = VGG19()
 
-    print(content_loss(model, base_img_resized ,content_layer=0))
+    target = base_img_resized.clone().requires_grad_(True)
+    optimizer = torch.optim.Adam([target], lr=0.03)
 
+    vgg = VGG19().to('cpu').eval()
 
+    # 9126.44554066658 seconds for:
+    # learning rate: 0.001
+    # 5000 epochs
 
+    for epoch in range(100):
+        print('Epoch: {}'.format(epoch))
+        
+        target_features = vgg(target)
+        base_features = vgg(base_img_resized)
+        style_features = vgg(style_img_resized)
 
+        style_loss = 0
+        base_loss = 0
 
-    # total_steps = 100
-    # learning_rate = 0.001
-    # alpha = 1
-    # beta = 0.01
-    # optimizer = optim.Adam([noise_img], lr = learning_rate)
+        for tf, bf, sf in zip(target_features, base_features, style_features):
+            base_loss += torch.mean((tf - bf) ** 2)
 
-    # for step in range(total_steps):
-    #     noise_features = model(noise_img)
-    #     base_features = model(base_img_resized)
-    #     style_features = model(style_img_resized)
+            _, c, h, w = tf.size()
+            tf = tf.view(c, h * w)
+            sf = sf.view(c, h * w)
 
-    #     style_loss = 0
-    #     base_loss = 0
-    #     print('Starting: {}'.format(step))
-    #     for noise_feature, base_feature, style_feature in zip(noise_features, base_features, style_features):
-    #         _, c, h, w = noise_feature.shape
-            
-    #         base_loss += torch.mean((noise_feature - base_feature) ** 2)
+            tf = torch.mm(tf, tf.t())
+            sf = torch.mm(sf, sf.t())
 
-    #         G = noise_feature.view(c, h * w).mm(noise_feature.view(c, h * w).t())
-    #         A = style_feature.view(c, h * w).mm(style_feature.view(c, h * w).t())
+            style_loss += torch.mean((tf - sf) ** 2 / (c * h * w))
 
-        #     style_loss += torch.mean((G - A) ** 2)
+        total_loss = 1e3 * base_loss + 1e6 * style_loss
+        optimizer.zero_grad()
+        total_loss.backward()
+        optimizer.step()
+        
+        
+    denorm = transforms.Normalize((-2.12, -2.04, -1.80), (4.37, 4.46, 4.44))
+    target_img = denorm(target.squeeze()).clamp_(0, 1)
+    final = transforms.ToPILImage()(target_img)
 
-        # total_loss = alpha * base_loss + beta * style_loss
-        # optimizer.zero_grad()
-        # total_loss.backward()
-        # optimizer.step()
-        # if step % 20 == 0:
-        #     denorm = transforms.Normalize((-2.12, -2.04, -1.80), (4.37, 4.46, 4.44))
-        #     stylized_img = noise_img.clone().squeeze()
-        #     stylized_img = denorm(stylized_img).clamp_(0, 1)
-        #     final = transforms.ToPILImage()(stylized_img) # make sure tensor is on cpu
-        #     final.show()
-
-
-        #     bytes_image = io.BytesIO()
-        #     final.save(bytes_image, 'JPEG')
-        #     bytes_image.seek(0)
-        #     print(total_loss)
-        #     print(stylized_img.shape)
-        #     print('YESSS')
-
-    # final = transforms.ToPILImage()(stylized_img) # make sure tensor is on cpu
-    # final.show()
+    bytes_image = io.BytesIO()
+    final.save(bytes_image, 'JPEG')
+    bytes_image.seek(0)
+    print(time.time() - start_time)
 
 
-    # bytes_image = io.BytesIO()
-    # final.save(bytes_image, 'JPEG')
-    # bytes_image.seek(0)
-
-
-    # return StreamingResponse(content=bytes_image, media_type="image/jpeg")
+    return StreamingResponse(content=bytes_image, media_type="image/jpeg")
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useEffect} from "react";
+import {useState} from "react";
 
 function App() {
     const [baseImagePreview, setBaseImagePreview] = useState(logo);
@@ -8,6 +8,8 @@ function App() {
     const [styleImagePreview, setStyleImagePreview] = useState(logo);
     const [styleImage, setStyleImage] = useState(null);
     const [stylizedImage, setStylizedImage] = useState(logo);
+    const [downloadLink, setDownloadLink] = useState(null);
+    const [loading, setLoading] = useState(false)
 
 
     function setBaseImageHandler(event){
@@ -24,7 +26,19 @@ function App() {
         setStyleImagePreview(URL.createObjectURL(event.target.files[0]))
     }
 
+    function handleDownload(event){
+        event.preventDefault();
+        let link = document.createElement('a');
+        link.download = 'style-thief-image.jpeg';
+        link.href = downloadLink;
+        link.click();
+        URL.revokeObjectURL(link.href);
+
+    }
+
+
     function handleOnSubmit(event){
+        setLoading(true)
         const formData = new FormData();
         console.log('base image: ' + baseImagePreview)
         console.log('style image: ' + styleImagePreview)
@@ -51,8 +65,11 @@ function App() {
         fetch('http://localhost:8000/upload', requestOptions)
         .then(response => response.blob())
         .then(blob => {
-            console.log(blob)
+
             let blobURL = URL.createObjectURL(blob);
+            let linkURL = window.URL.createObjectURL(blob)
+            
+            setDownloadLink(linkURL);
             setStylizedImage(blobURL)
             let image = document.getElementById('stylized-image')
             image.onload = function(){
@@ -62,12 +79,14 @@ function App() {
             image.src = blobURL;
             setStylizedImage(blobURL)
             console.log(stylizedImage)
+            setLoading(false)
 
 
         })
         .catch(error => {
             console.log(error)
         })
+        
 
     }
 
@@ -81,22 +100,26 @@ function App() {
         <div className={'mt-5 py-20 grid grid-cols-4 gap-4 content-center'}>
 
             <div className={'aspect-w-3 aspect-h-3 inline-block ml-5'}>
-                <img className={'border-2 rounded'} height={400} width={400} alt={'base image'} src={baseImagePreview}/>
+                <img className={'border-2 rounded'} height={400} width={400} alt={'base'} src={baseImagePreview}/>
 
             </div>
 
             <div className={'aspect-w-3 aspect-h-3 inline-block ml-5'}>
-                <img className={'border-2 rounded'} height={400} width={400} alt={'style image'} src={styleImagePreview}/>
+                <img className={'border-2 rounded'} height={400} width={400} alt={'style'} src={styleImagePreview}/>
 
             </div>
 
             <div/>
-
-
+            {loading === true ? 
+            <span className={"loader"}></span>
+            :
             <div className={'aspect-w-3 aspect-h-3 inline-block border-2 mr-5'}>
-                <img id={'stylized-image'} className={'border-2 rounded'} height={400} width={400} alt={'stylized-image'} src={stylizedImage}/>
-
+                <img id={'stylized-image'} className={'border-2 rounded'} height={400} width={400} alt={'stylized'} src={stylizedImage}/>
             </div>
+
+
+            }
+            
 
         </div>
 
@@ -130,11 +153,12 @@ function App() {
 
             </div>
 
-            <div className={'ml-5 hover:-translate-y-0.5'}>
-                <label htmlFor="style-image" className={'border-2 cursor-pointer px-2 hover:text-sky-400'}>Download Stylized
+            <div id={'stylized-image-button'} className={'ml-5 hover:-translate-y-0.5'}>
+                <button onClick={handleDownload}>Download Image</button>
+                {/* <label htmlFor="stylized-image-button" className={'border-2 cursor-pointer px-2 hover:text-sky-400'}>Download Stylized
                     Image</label>
-                <input id={'style-image'} type={'file'} className={'hidden'} onChange={null}
-                       placeholder={''}/>
+                <a download id={'stylized-image-button'} type={'file'} className={'hidden'} onChange={null} href={stylizedImage}
+                       placeholder={''}/> */}
             </div>
 
 
